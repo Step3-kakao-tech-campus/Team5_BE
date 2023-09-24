67eb6935dd9c636d4829dcf3e1b0bbd6a6672ac2
WoeunJung <leena09123@naver.com>
2023-09-25 01:28:09 +0900

    feat: BaseException(enum) added
    쉬운 에러 메시지 변경을 위해 전체에서 사용가능한 BaseException 코드 구현

---
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/_core/errors/BaseException.java
@@ -0,0 +1,20 @@
+package com.kakao.sunsuwedding._core.errors;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum BaseException {
+    USER_NOT_FOUND("회원이 존재하지 않습니다 :", 404),
+    USER_EMAIL_EXIST("동일한 이메일이 존재합니다 :", 400),
+    USER_ROLE_WRONG("role은 플래너, 또는 예비 부부만 가능합니다 :", 400),
+    USER_PASSWORD_NOT_SAME("패스워드1과 패스워드2는 동일해야 합니다: ", 400),
+    USER_UNEXPECTED_ERROR("[User] 예상치 못한 문제가 발생했습니다", 500);
+
+
+    @Getter
+    private final String message;
+
+    @Getter
+    private final int status;
+}
