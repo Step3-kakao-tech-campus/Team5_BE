6a3a0dcbdf2c3ee0692bae49aa36fff0e6cffe59
chae <chaee813@gmail.com>
2023-09-27 15:44:00 +0900

    fix: modified teardown.sql
    - 패스워드 필드값 raw password에서 암호화된 password로 변경

--- sunsu-wedding/src/main/resources/db/teardown.sql
+++ sunsu-wedding/src/main/resources/db/teardown.sql
@@ -5,8 +5,9 @@ truncate table couple_tb;
 --truncate table imageitem_tb;
 --truncate table priceitem_tb;
 --truncate table match_tb;
+SET REFERENTIAL_INTEGRITY TRUE;
 
-INSERT INTO planner_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('1','planner@gmail.com','planner1234!','planner','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','NORMAL');
-INSERT INTO planner_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('2','planner2@gmail.com','planner1234!','planner2','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','PREMIUM');
-INSERT INTO couple_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('1','couple@gmail.com','couple1234!','couple','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','NORMAL');
-INSERT INTO couple_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('2','couple2@gmail.com','couple1234!','couple2','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','PREMIUM');
+INSERT INTO planner_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('1','planner@gmail.com','{bcrypt}$2a$10$89SwVjyXVDhK3GFcN4c8Bu3kQlNiWqjaTvgiXaCi9D/1eWx2w7CBa','planner','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','NORMAL');
+INSERT INTO planner_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('2','planner2@gmail.com','{bcrypt}$2a$10$89SwVjyXVDhK3GFcN4c8Bu3kQlNiWqjaTvgiXaCi9D/1eWx2w7CBa','planner2','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','PREMIUM');
+INSERT INTO couple_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('1','couple@gmail.com','{bcrypt}$2a$10$bKgX34po45/xYw1Dd8C81OYW4dkkVQV5lHd7a.06m1gBX689XERA.','couple','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','NORMAL');
+INSERT INTO couple_tb (`id`,`email`,`password`,`username`,`created_at`,`payed_at`,`grade`) VALUES ('2','couple2@gmail.com','{bcrypt}$2a$10$bKgX34po45/xYw1Dd8C81OYW4dkkVQV5lHd7a.06m1gBX689XERA.','couple2','2023-09-16 01:06:55.00','2023-09-20 15:26:55.00','PREMIUM');
