14da1adc058f57f5ff67e0764c605252ddddccdb
seokwns <seokjun0915@icloud.com>
2023-10-01 21:33:59 +0900

    feat: add controller mapping
    - 파일 통합으로 인한 메서드 통합

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/portfolio/PortfolioRestController.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/portfolio/PortfolioRestController.java
@@ -2,9 +2,13 @@ package com.kakao.sunsuwedding.portfolio;
 
 import com.kakao.sunsuwedding._core.security.CustomUserDetails;
 import com.kakao.sunsuwedding._core.utils.ApiUtils;
+import com.kakao.sunsuwedding.portfolio.dto.response.PortfolioDTO;
+import com.kakao.sunsuwedding.portfolio.dto.response.PortfolioListItemDTO;
 import com.kakao.sunsuwedding.portfolio.image.ImageItemService;
 import com.kakao.sunsuwedding.user.planner.Planner;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.util.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +16,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class PortfolioRestController {
     private final PortfolioService portfolioService;
     private final ImageItemService imageItemService;
+    private static final int PAGE_SIZE = 10;
 
     @PostMapping(value = "/portfolios", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE} )
     public ResponseEntity<?> addPortfolios(@RequestPart PortfolioRequest.addDTO request,
@@ -27,6 +34,19 @@ public class PortfolioRestController {
         imageItemService.uploadImage(images, info.getFirst(), info.getSecond());
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+
+    @GetMapping(value = "/portfolios")
+    public ResponseEntity<?> getPortfolios(@RequestParam @Min(0) int page) {
+        PageRequest pageRequest = PageRequest.of(page, PAGE_SIZE);
+        List<PortfolioListItemDTO> items = portfolioService.getPortfolios(pageRequest);
+        return ResponseEntity.ok().body(ApiUtils.success(items));
+    }
+
+    @GetMapping("/portfolios/{id}")
+    public ResponseEntity<?> getPortfolioInDetail(@PathVariable @Min(1) Long id) {
+        PortfolioDTO portfolio = portfolioService.getPortfolioById(id);
+        return ResponseEntity.ok().body(ApiUtils.success(portfolio));
     }
 
     @PutMapping(value = "/portfolios", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE} )
@@ -42,7 +62,9 @@ public class PortfolioRestController {
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
-
-
-
+    @DeleteMapping("/portfolios")
+    public ResponseEntity<?> deletePortfolio(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        portfolioService.deletePortfolio(userDetails.getInfo());
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
