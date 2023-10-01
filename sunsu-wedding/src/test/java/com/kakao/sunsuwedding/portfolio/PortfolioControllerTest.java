7a1dd764af636597793c9fc1ee9f39f915aaa198
seokwns <seokjun0915@icloud.com>
2023-10-01 16:52:43 +0900

    fix: delete unnecessary annotations

--- sunsu-wedding/src/test/java/com/kakao/sunsuwedding/portfolio/PortfolioControllerTest.java
+++ sunsu-wedding/src/test/java/com/kakao/sunsuwedding/portfolio/PortfolioControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -21,7 +20,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
         SecurityConfig.class
 })
 @Sql("classpath:/db/teardown.sql")
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class PortfolioControllerTest {
