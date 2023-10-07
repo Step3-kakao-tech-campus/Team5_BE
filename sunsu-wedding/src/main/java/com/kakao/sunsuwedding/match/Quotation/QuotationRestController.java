166d0a8d46ade012b0656c738fbaccc06597a9a0
seokwns <seokjun0915@icloud.com>
2023-10-08 08:47:50 +0900

    feat: add request validation
    - 요청 유효성 검증 추가

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/Quotation/QuotationRestController.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/Quotation/QuotationRestController.java
@@ -2,6 +2,7 @@ package com.kakao.sunsuwedding.match.Quotation;
 
 import com.kakao.sunsuwedding._core.security.CustomUserDetails;
 import com.kakao.sunsuwedding._core.utils.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,7 +16,7 @@ public class QuotationRestController {
     @PostMapping("/quotations")
     public ResponseEntity<?> createQuotation(@AuthenticationPrincipal CustomUserDetails userDetails,
                                              @RequestParam Long matchId,
-                                             @RequestBody QuotationRequest.addQuotation request) {
+                                             @Valid @RequestBody QuotationRequest.addQuotation request) {
         quotationService.insertQuotation(userDetails.getInfo(), matchId, request);
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
