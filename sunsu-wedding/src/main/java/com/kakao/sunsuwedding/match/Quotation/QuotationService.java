eeccb828c22775a7d8bb4256cfbf2d821d5207f5
seokwns <seokjun0915@icloud.com>
2023-10-08 21:19:01 +0900

    refactor: change match status check method
    - match 상태 체크 로직 변경
    - MatchStatus 에서 문자열로 변환하는 메서드 추가

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/Quotation/QuotationService.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/Quotation/QuotationService.java
@@ -5,6 +5,7 @@ import com.kakao.sunsuwedding._core.errors.exception.Exception403;
 import com.kakao.sunsuwedding._core.errors.exception.Exception404;
 import com.kakao.sunsuwedding.match.Match;
 import com.kakao.sunsuwedding.match.MatchJPARepository;
+import com.kakao.sunsuwedding.match.MatchStatus;
 import com.kakao.sunsuwedding.user.constant.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
@@ -42,21 +43,10 @@ public class QuotationService {
                 .orElseThrow(() -> new Exception404(BaseException.MATCHING_NOT_FOUND.getMessage()));
 
         List<Quotation> quotations = quotationJPARepository.findAllByMatch(match);
-        QuotationStatus status = getEntireQuotationStatus(quotations);
+        MatchStatus status = match.getStatus();
 
         List<QuotationResponse.QuotationDTO> quotationDTOS = QuotationDTOConverter.toFindByMatchIdDTO(quotations);
 
         return new QuotationResponse.findAllByMatchId(status.toString(), quotationDTOS);
-    }
-
-    private static QuotationStatus getEntireQuotationStatus(List<Quotation> quotations) {
-        QuotationStatus status = QuotationStatus.CONFIRMED;
-        for (Quotation quotation : quotations) {
-            if (quotation.getStatus().equals(QuotationStatus.UNCONFIRMED)) {
-                status = QuotationStatus.UNCONFIRMED;
-                break;
-            }
-        }
-        return status;
     }
 }
