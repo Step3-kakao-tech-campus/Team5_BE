a804d923a71afeaeaf15d02cdac25cc88cdd19f8
chae <chaee813@gmail.com>
2023-10-19 17:22:36 +0900

    refacor: add constraint on PortfolioRequest
    - 포트폴리오 요청 DTO에 제약사항 추가

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/portfolio/PortfolioRequest.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/portfolio/PortfolioRequest.java
@@ -1,5 +1,6 @@
 package com.kakao.sunsuwedding.portfolio;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,18 +10,32 @@ import java.util.List;
 public class PortfolioRequest {
     @Getter @Setter @ToString
     public static class AddDTO {
+        @NotNull(message = "plannerName은 비어있으면 안됩니다.")
         private String plannerName;
+
+        @NotNull(message = "title은 비어있으면 안됩니다.")
         private String title;
+
+        @NotNull(message = "description은 비어있으면 안됩니다.")
         private String description;
+
+        @NotNull(message = "location은 비어있으면 안됩니다.")
         private String location;
+
+        @NotNull(message = "career는 비어있으면 안됩니다.")
         private String career;
+
+        @NotNull(message = "partnerCompany는 비어있으면 안됩니다.")
         private String partnerCompany;
 
         private List<ItemDTO> items;
 
         @Getter @Setter @ToString
         public static class ItemDTO {
+            @NotNull(message = "itemTitle은 비어있으면 안됩니다.")
             private String itemTitle;
+
+            @NotNull(message = "itemPrice는 비어있으면 안됩니다.")
             private Long itemPrice;
         }
 
@@ -28,18 +43,32 @@ public class PortfolioRequest {
 
     @Getter @Setter @ToString
     public static class UpdateDTO {
+        @NotNull(message = "plannerName은 비어있으면 안됩니다.")
         private String plannerName;
+
+        @NotNull(message = "title은 비어있으면 안됩니다.")
         private String title;
+
+        @NotNull(message = "description은 비어있으면 안됩니다.")
         private String description;
+
+        @NotNull(message = "location은 비어있으면 안됩니다.")
         private String location;
+
+        @NotNull(message = "career는 비어있으면 안됩니다.")
         private String career;
+
+        @NotNull(message = "partnerCompany는 비어있으면 안됩니다.")
         private String partnerCompany;
 
         private List<ItemDTO> items;
 
         @Getter @Setter @ToString
         public static class ItemDTO {
+            @NotNull(message = "itemTitle은 비어있으면 안됩니다.")
             private String itemTitle;
+
+            @NotNull(message = "itemPrice는 비어있으면 안됩니다.")
             private Long itemPrice;
         }
 
