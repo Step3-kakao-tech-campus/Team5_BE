f6685e2eda62d11a13deae8c4b61d238d66fe941
chae <chaee813@gmail.com>
2023-10-29 00:45:15 +0900

    refactor: delete unused import

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/MatchRestController.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/match/MatchRestController.java
@@ -2,12 +2,14 @@ package com.kakao.sunsuwedding.match;
 
 import com.kakao.sunsuwedding._core.security.CustomUserDetails;
 import com.kakao.sunsuwedding._core.utils.ApiUtils;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
