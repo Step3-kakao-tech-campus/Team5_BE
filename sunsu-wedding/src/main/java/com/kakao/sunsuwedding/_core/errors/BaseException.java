a5d8d241b3b48328225f907f8b2ca15940b1f8d0
seokwns <seokjun0915@icloud.com>
2023-10-29 15:02:06 +0900

    fix: test error
    - 에러 메시지 양식 수정
    - 테스트에서 발생하는 에러 수정

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/_core/errors/BaseException.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/_core/errors/BaseException.java
@@ -8,18 +8,18 @@ import lombok.RequiredArgsConstructor;
 public enum BaseException {
     // 공통 (1000~)
     PERMISSION_DENIED_METHOD_ACCESS("사용할 수 없는 기능입니다.", 1000, 403),
-    DATABASE_ERROR("데이터베이스 에러입니다", 1001, 500),
+    DATABASE_ERROR("데이터베이스 에러입니다.", 1001, 500),
 
     // 안 쓰는 부분은 삭제하기
     // user 관련
     // 2000 번대, 1씩 올라가도록
     USER_NOT_FOUND("서비스를 탈퇴했거나 가입하지 않은 유저의 요청입니다.", 2000, 404),
-    PLANNER_NOT_FOUND("서비스를 탈퇴하거나 가입하지 않은 플래너입니다", 2001, 404),
+    PLANNER_NOT_FOUND("서비스를 탈퇴하거나 가입하지 않은 플래너입니다.", 2001, 404),
     USER_EMAIL_EXIST("동일한 이메일이 존재합니다.", 2002, 400),
-    USER_EMAIL_NOT_FOUND("이메일을 찾을 수 없습니다 : ", 2003, 400),
+    USER_EMAIL_NOT_FOUND("이메일을 찾을 수 없습니다.", 2003, 400),
     USER_ROLE_WRONG("role은 플래너, 또는 예비 부부만 가능합니다.", 2004, 400),
-    USER_PASSWORD_WRONG("패스워드를 잘못 입력하셨습니다", 2005, 400),
-    USER_PASSWORD_NOT_SAME("패스워드1과 패스워드2는 동일해야 합니다.: ", 2006, 400),
+    USER_PASSWORD_WRONG("패스워드를 잘못 입력하셨습니다.", 2005, 400),
+    USER_PASSWORD_NOT_SAME("패스워드1과 패스워드2는 동일해야 합니다.", 2006, 400),
     USER_ALREADY_PREMIUM("이미 프리미엄 회원입니다.", 2007, 400),
     USER_UNEXPECTED_ERROR("[User] 예상치 못한 문제가 발생했습니다.", 2008, 500),
     USER_PERMISSION_DENIED("권한이 없습니다.", 2009, 403),
@@ -31,7 +31,7 @@ public enum BaseException {
     ACCESS_TOKEN_STILL_ALIVE("액세스 토큰이 아직 유효합니다. 액세스 토큰만 가지고 접근해주세요.", 2101, 401),
     ALL_TOKEN_EXPIRED("모든 토큰이 만료되었습니다. 다시 로그인 해야합니다.", 2102, 401),
     TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다. 다시 로그인해주세요.", 2103, 404),
-    TOKEN_NOT_VALID("로그인 토큰이 유효하지 않습니다. 다시 로그인 해주세요", 2104, 401),
+    TOKEN_NOT_VALID("로그인 토큰이 유효하지 않습니다. 다시 로그인 해주세요.", 2104, 401),
     TOKEN_REFRESH_FORBIDDEN("토큰을 갱신할 수 없습니다.", 2105, 403),
 
     // 결제 관련 3000 번대
@@ -57,7 +57,7 @@ public enum BaseException {
 
     // 견적서 관련 6000
     QUOTATIONS_NOT_ALL_CONFIRMED("확정되지 않은 견적서가 있습니다.", 6000, 400),
-    QUOTATION_NOTHING_TO_CONFIRM("확정할 견적서가 없습니다", 6001, 400),
+    QUOTATION_NOTHING_TO_CONFIRM("확정할 견적서가 없습니다.", 6001, 400),
     QUOTATION_NOT_FOUND("해당 견적서를 찾을 수 없습니다.", 6002, 404),
     QUOTATION_ACCESS_DENIED("해당 매칭 내역에 접근할 수 없습니다.", 6003, 403),
     QUOTATION_ALREADY_CONFIRMED("견적서가 확정된 상태입니다.",6004, 403), // 수정 필요 400
