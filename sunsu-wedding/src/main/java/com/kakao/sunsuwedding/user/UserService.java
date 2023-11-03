a6fd5ada969d80c8daf8997fcce386059bf6ee27
WoeunJung <leena09123@naver.com>
2023-11-04 03:18:30 +0900

    refactor: user role exception removed

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/user/UserService.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/user/UserService.java
@@ -40,24 +40,19 @@ public class UserService {
         sameCheckPassword(requestDTO.getPassword(), requestDTO.getPassword2());
         requestDTO.setPassword(passwordEncoder.encode(requestDTO.getPassword()));
         Role role = Role.valueOfRole(requestDTO.getRole());
-        try {
-            if (role == Role.COUPLE) {
-                coupleJPARepository.save(requestDTO.toCoupleEntity());
-            }
-            else {
-                plannerJPARepository.save(requestDTO.toPlannerEntity());
-            }
-        } catch (Exception e) {
-            throw new ServerException(BaseException.USER_UNEXPECTED_ERROR);
+        if (role == Role.COUPLE) {
+            coupleJPARepository.save(requestDTO.toCoupleEntity());
+        }
+        else {
+            plannerJPARepository.save(requestDTO.toPlannerEntity());
         }
     }
 
     @Transactional
     public TokenDTO login(UserRequest.LoginDTO requestDTO) {
         User user = userJPARepository.findByEmail(requestDTO.getEmail()).orElseThrow(
-                () -> new BadRequestException(BaseException.USER_EMAIL_NOT_FOUND.getMessage() + requestDTO.getEmail())
+                () -> new BadRequestException(BaseException.USER_EMAIL_NOT_FOUND)
         );
-        log.debug("디버그: 로그인 토큰 {}", user.getId());
         if (!passwordEncoder.matches(requestDTO.getPassword(), user.getPassword())) {
             throw new BadRequestException(BaseException.USER_PASSWORD_WRONG);
         }
