60c49d91f66756b7e03e404a6293e5ea982887c8
WoeunJung <leena09123@naver.com>
2023-11-04 03:16:31 +0900

    feat: remove unused payment apis
    - 사용하지 않는 결제 관련 API 삭제(검증, 업그레이드 API)

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/payment/PaymentRequest.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/payment/PaymentRequest.java
@@ -19,35 +19,6 @@ public class PaymentRequest {
 
     @Getter
     @Setter
-    public static class ConfirmDTO{
-        @NotEmpty(message = "orderId는 비어있으면 안됩니다.")
-        private String orderId;
-
-        @NotEmpty(message = "paymentKey는 비어있으면 안됩니다.")
-        private String paymentKey;
-
-        @Min(value = 0, message = "금액은 양수여야 합니다.")
-        private Long amount;
-    }
-
-    @Getter
-    @Setter
-    public static class UpgradeDTO{
-        @NotEmpty(message = "orderId는 비어있으면 안됩니다.")
-        private String orderId;
-
-        @NotEmpty(message = "paymentKey는 비어있으면 안됩니다.")
-        private String paymentKey;
-
-        @NotEmpty(message = "상태값은 비어있으면 안됩니다.")
-        private String status;
-
-        @Min(value = 0, message = "금액은 양수여야 합니다.")
-        private Long amount;
-    }
-
-    @Getter
-    @Setter
     public static class ApproveDTO{
         @NotEmpty(message = "orderId는 비어있으면 안됩니다.")
         private String orderId;
