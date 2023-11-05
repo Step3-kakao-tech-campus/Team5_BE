1fe761d3e3982ba73bedb4b2271103989714d284
chae <chaee813@gmail.com>
2023-11-06 03:58:08 +0900

    refactor: modify mail api

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/user/mail/MailCodeController.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/user/mail/MailCodeController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/mail")
+@RequestMapping("/api/mail")
 @RequiredArgsConstructor
 public class MailCodeController {
     private final MailService mailService;
